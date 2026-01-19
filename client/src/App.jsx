ed6c5a93e23677b25322682b230f17fdd759ca04
tiffany707 <tiffanyokura7@gmail.com>
2026-01-18 18:05:35 -0700

    feat: Add page formatting, Updated Scraper to return information from given Url

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,11 +6,15 @@ function App() {
 
   const [Url, setUrl] = useState("")
   const [info, setInfo] = useState(null)
+  const [loading, setLoading] = useState("Clean")
 
   const getRecipe = async (e) => {
     e.preventDefault();
     try{
       const response = await axios.post("/scraper", {text:Url});
+      response.data.directions = response.data.directions.filter(items => !items.toLowerCase().includes("dotdash meredith food studios"));
+      console.log(response.data.details)
+      setLoading("Clean")
       setInfo(response.data)
     }
     catch(e){
@@ -18,25 +22,57 @@ function App() {
     }
   }  
 
+  const changeLoading = () =>{
+    setLoading("Loading...")
+  }
+
   return (
     <>
     <div className="flex pt-20 items-center w-full min-h-screen flex-col  bg-gray-50">
 
-      <div className="px-6 py-4 rounded-2xl w-fit shadow-2xl">
-        <h1 className="flex justify-center items-center pb-3">Recipe Cleaner</h1>
-        <form onSubmit={getRecipe}>
-          <input value={Url} onChange={(e)=>{setUrl(e.target.value)}} className="border border-gray-300 rounded-lg py-2 px-4" type="text" placeholder="Enter URL"/>
-          <button className="bg-blue-400 rounded-lg py-2 px-4 text-white hover:bg-blue-500 cursor-pointer"type="submit">Clean</button>
+      <div className="px-30 py-4 rounded-xl w-fit shadow-2xl">
+        <h1 className="flex justify-center items-center pb-3 text-2xl font-smi-bold">Recipe Cleaner</h1>
+        <h2 className="text-red-800 text-sm flex justify-center mb-1">Disclaimer: Only works with allrecipes.com</h2>
+        <form onSubmit={(e)=>{getRecipe(e); changeLoading();}} className="flex flex-col md:flex-row items-center justify-center">
+          <input required value={Url} onChange={(e)=>{setUrl(e.target.value)}} className="border border-gray-300 rounded-lg py-2 px-2 w-100 mr-2" type="text" placeholder="Enter URL"/>
+          <button className="mt-2 sm:mt-0 bg-blue-400 rounded-lg py-2 px-4 text-white hover:bg-blue-500 transition-colors duration-200 cursor-pointer"type="submit">{loading}</button>
         </form>
       </div>
       {/* Displaying info retrieved */}
-      <div>{info ?
-        (<div className="px-20 pt-7 rounded-lg shadow-lg flex-box justify-center items-center">
+      <div className="mt-1 sm:mt-5 mx-6 py-20">{info ?
+        (<div className="px-12 sm:px-20 pt-7 rounded-lg shadow-xl flex-box justify-center items-center">
           <h1 className="font-semibold text-3xl">{info.title}</h1>
-          <div className="py-3"><h3 className="font-semibold text-lg">Details</h3>{info.details} </div>
-          <div className="py-3"><h3 className="font-semibold text-lg">Ingredients</h3>{info.ingredients} </div>
-          <div className="py-3"><h3 className="font-semibold text-lg">Garnish</h3>{info.garnish} </div>
-          <div className="py-3"><h3 className="font-semibold text-lg">Directions</h3>{info.directions} </div>
+          <div className="py-3 m-2"><h3 className="font-semibold text-xl mb-3">Details</h3>
+            <table>
+              <thead className="pt-2">
+                {info.details.map((items,index) =>(
+                index % 3 == 0 ?
+                  (<tr className="border-t-2 border-gray-300">
+                    <td className="p-3">
+                      {items}
+                    </td>
+                  </tr>):
+                  null
+                ))} 
+              </thead>
+            </table>
+          </div>
+          <div className="py-3"><h3 className="font-semibold text-xl">Ingredients</h3>
+            <ul className="list-disc ml-5 pt-2">
+              {info.ingredients.map((items, index) => {
+              return (<li key={index + 100}>{items}</li>)
+              })} 
+            </ul>
+          </div>
+          <div className="pt-3 pb-20 mt-5"><h3 className="font-semibold text-xl">Directions</h3>
+          {info.directions.map((items, index)=>(
+          <div className="pt-3">
+            <div className="font-semibold">Step {index + 1}</div>
+            <div>{items}</div>
+          </div>
+          ))}
+          
+          </div>
         </div>
 
 
--- server/router.js
+++ server/router.js
@@ -3,22 +3,20 @@ import { chromium } from "playwright";
 
 const router = express.Router();
 
-const runScraper = async () => {
+const runScraper = async (url) => {
     try{
-        const browser = await chromium.launch( {headless: false, slowMo: 1000} );
+        const browser = await chromium.launch();
         const context = await browser.newContext();
         const page = await context.newPage();
-        await page.goto("https://www.allrecipes.com/recipe/31144/sweet-and-sour-pork-iii/");
+        await page.goto(url);
         const title = await page.locator(".article-heading.text-headline-400").innerText();
-        const details = await page.locator(".mm-recipes-details__content").innerText();
+        const details = await page.locator(".mm-recipes-details__content div").allInnerTexts();
         let ingredients = await page.locator('.comp.mm-recipes-lrs-ingredients.mntl-block li').allInnerTexts();
-        ingredients = ingredients.join('\n');
-        const garnish = await page.locator('.mm-recipes-structured-ingredients__list').nth(1).innerText();
-        const directions = await page.locator(".comp.mntl-sc-block.mntl-sc-block-startgroup.mntl-sc-block-group--OL").allInnerTexts();
+        const directions = await page.locator(".comp.mntl-sc-block.mntl-sc-block-startgroup.mntl-sc-block-group--OL p").allInnerTexts();
         console.log("hi im scraper")
         
         await browser.close();
-        return [title, details, ingredients, garnish, directions];
+        return [title, details, ingredients, directions];
     }
     catch(e){
         console.log(e.message);
@@ -28,13 +26,12 @@ const runScraper = async () => {
 
 router.post('/', async (req, res) =>{
     try{
-        const data = await runScraper();
+        const data = await runScraper(req.body.text);
         res.json({
             title: data[0],
             details: data[1],
             ingredients: data[2],
-            garnish: data[3],
-            directions: data[4]
+            directions: data[3]
         });
     }catch(e){
         res.status(500).send(`
